459e16fd2c810c7843d80203205c9228e50ffd59
Kõpskink <154380199+Kopskink@users.noreply.github.com>
2025-08-30 18:56:51 +0300

    Create ! items.lua

---
+++ [EXTRA]/inventory/! items.lua
@@ -0,0 +1,63 @@
+-- NICOTINE POUCHES
+
+["tupsukarp"] = {
+    label = "Garant Extreme",
+    weight = 27,
+    stack = true,
+    close = false,
+    description = "Nikotiinpatjade karp. 19.8mg / padja kohta",
+    client = {
+        image = "tupsukarp.png",
+    }
+},
+
+["tups"] = {
+    label = "Tups",
+    weight = 1,
+    stack = true,
+    close = true,
+    description = "Nikotiinipadi 19.8mg / padja kohta",
+    client = {
+        image = "tups.png",
+    }
+},
+
+-- ADRENALINE SYRINGE
+
+["adresys"] = {
+    label = "Adrenaliini süstal",
+    weight = 20,
+    stack = true,
+    close = true,
+    description = "Kiire lahendus surma äärel, too sõber tagasi ellu nagu tõeline kangelane.",
+    client = {
+        image = "adresys.png",
+    }
+},
+
+-- HANDCUFFS
+
+["newhandcuffs"] = {
+	label = "Käerauad",
+	weight = 100,
+	stack = true,
+	close = true,
+	description = "Ühendavad inimesi, aga mitte just vabatahtlikult.",
+	client = {
+		image = "newhandcuffs.png",
+	}
+},
+
+-- WIRECUTTERS
+
+["wirecutters"] = {
+	label = "Traaditangid",
+	weight = 500,
+	stack = false,
+	close = true,
+	decay = true,
+	description = "Lõikavad toksilised sidemed läbi kiiremini kui keskmine armastusnõustaja.",
+	client = {
+		image = "wirecutters.png",
+	}
+},
